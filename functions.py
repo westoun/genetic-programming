6e9918a9e1702a4dff000a1f5392d389c3bb0e02
westoun <christoph.werner.stein@gmail.com>
2025-03-07 11:01:16 +0100

    Move nodes and math functions to separate files

---
+++ functions.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def divide(a, b):
+    if b == 0:
+        return 100000
+
+    return a / b
+
+
+def pow(a, b):
+    return a**b
